2cc3d7b3e4ac9c171e5c8a48e22e0af429e6d938
Brad Feehan <git@bradfeehan.com>
2014-05-05 16:41:01 +1000

    Add unit test for FileFilterIterator

--- tests/Test/Helper/UnitTestCase.php
+++ tests/Test/Helper/UnitTestCase.php
@@ -2,6 +2,8 @@
 
 namespace BradFeehan\GuzzleModularServiceDescriptions\Test\Helper;
 
+use UnexpectedValueException;
+
 /**
  * Base class for all unit test cases
  *
@@ -9,4 +11,48 @@ namespace BradFeehan\GuzzleModularServiceDescriptions\Test\Helper;
  */
 abstract class UnitTestCase extends TestCase
 {
+
+    /**
+     * Determines the name of the class to mock with $this->mock()
+     *
+     * This implementation tries to guess the class name to use. If
+     * it's wrong, it should be re-implemented in the child class.
+     *
+     * @return string
+     */
+    protected function getMockedClass()
+    {
+        $className = preg_replace(
+            array(
+                // Patterns
+                '#^BradFeehan\\\\GuzzleModularServiceDescriptions\\\\Test\\\\Unit#',
+                '#Test$#',
+            ),
+            array(
+                // Replacements (order corresponds to patterns above)
+                'BradFeehan\\\\GuzzleModularServiceDescriptions',
+                '',
+            ),
+            get_class($this)
+        );
+
+        if (!class_exists($className)) {
+            throw new UnexpectedValueException(
+                "Couldn't guess mock class for " . get_class($this) .
+                ", guess of '$className' isn't defined"
+            );
+        }
+
+        return $className;
+    }
+
+    /**
+     * Retrieves a mocked instance of $this->getMockedClass()
+     *
+     * @return \Mockery\MockInterface
+     */
+    protected function mock()
+    {
+        return \Mockery::mock($this->getMockedClass());
+    }
 }
